b8d26e3e0e2fca12da612a39075085823319c679
lyzno1 <yuanyouhuilyz@gmail.com>
2025-04-12 13:12:18 +0800

    继续瘦身

--- utils/app/clean.ts
+++ utils/app/clean.ts
@@ -12,13 +12,6 @@ export const cleanSelectedConversation = (conversation: Conversation) => {
 
   let updatedConversation = conversation;
 
-  // check for model on each conversation
-  if (!updatedConversation.model) {
-    updatedConversation = {
-      ...updatedConversation,
-      model: updatedConversation.model || OpenAIModels[OpenAIModelID.GPT_3_5],
-    };
-  }
 
   // check for system prompt on each conversation
   if (!updatedConversation.prompt) {
